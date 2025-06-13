1569390f51c2a748843c87b63365cd17cbe87f52
3dw4rd-sn0wd3n <83455051+3dw4rd-sn0wd3n@users.noreply.github.com>
2025-06-13 09:39:38 +0500

    Add files via upload

---
+++ rpc_script.py
@@ -0,0 +1,15 @@
+from pypresence import Presence
+import json
+import time
+
+# Загружаем конфиг
+with open("rests.json", "r") as file:
+    config = json.load(file)
+
+client_id = config["clientID"]  # ID приложения Discord
+rpc = Presence(client_id)
+rpc.connect()
+
+while True:
+    rpc.update(**config["data"])  # Обновляем статус
+    time.sleep(15)  # Обновление каждые 15 секунд
